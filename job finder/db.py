797bc9a6bf17ae4e7c1076f1303ff96824f53e32
Eric Warren <ericwarren99@hotmail.com>
2020-10-17 14:43:18 -0400

    merged like mclasses like they should have been

--- job finder/db.py
+++ job finder/db.py
@@ -2,16 +2,21 @@ from sqlalchemy import create_engine , Column , Boolean, String, Integer, Table,
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import database_exists, create_database
+from .objects import job_c, company_c
 
-
+# This Functions is to create the DB and tables if it does not already exists
 def create_db():
+    # Checks if the DB file job_db.db exsists and if it doesnt creates it
     if not database_exists("sqlite:///job_db.db"):
         create_database("sqlite:///job_db.db")
+
+    # This is needed for SQL alchemy 
     engine = create_engine("sqlite:///job_db.db")
 
-    if not engine.dialect.has_table(engine, "jobs"):  # If table don't exist, Create.
+    # Creates the jobs table if it does not exists
+    if not engine.dialect.has_table(engine, "jobs"):  
         metadata = MetaData(engine)
-        # Create a table with the appropriate Columns
+        # Create a table object with the appropriate Columns
         Table("jobs", metadata,
             Column('md5', String, primary_key=True), 
             Column('isremote', Boolean), Column('company', String),
@@ -23,46 +28,12 @@ def create_db():
 
     if not engine.dialect.has_table(engine, "companies"):  # If table don't exist, Create.
         metadata = MetaData(engine)
-        # Create a table with the appropriate Columns
+        # Create a table object with the appropriate Columns
         Table("companies", metadata,
             Column('name', String, primary_key=True))
         # Implement the creation
         metadata.create_all()
 
-
-base = declarative_base()
-
-class db_job(base):
-    
-    __tablename__ = "jobs"
-
-    md5 = Column(String, primary_key=True)
-    isremote = Column(Boolean)
-    company = Column(String)
-    description = Column(String)
-    city = Column(String)
-    salary_low = Column(Integer)
-    salary_high = Column(Integer)
-    us_only = Column(Boolean)
-
-    def __init__(self, job):
-        self.md5 = job.md5
-        self.isremote = job.isremote
-        self.company = job.company
-        self.description = job.description
-        self.city = job.city
-        self.salary_low = job.salary_low
-        self.salary_high = job.salary_high
-        self.us_only = job.us_only
-
-class db_company(base):
-
-    __tablename__ = "companies"
-
-    name = Column(String, primary_key=True)
-
-    def __init__(self, comp):
-        self.name = comp.name
 
 def insert_job(job):
     all_jobs = get_jobs()
@@ -72,26 +43,26 @@ def insert_job(job):
         engine = create_engine("sqlite:///job_db.db")
         session = sessionmaker(bind=engine)()
         
-        session.add(db_job(job))
+        session.add(job)
         session.commit()
 
 def insert_company(company):
 
     engine = create_engine("sqlite:///job_db.db")
     session = sessionmaker(bind=engine)()
-    session.add(db_company(company))
+    session.add(company))
     session.commit()
 
 def get_jobs():
     engine = create_engine("sqlite:///job_db.db")
     session = sessionmaker(bind=engine)()
-    jobs = session.query(db_job).all()
+    jobs = session.query(job_c).all()
     return jobs
 
 def get_companies():
     engine = create_engine("sqlite:///job_db.db")
     session = sessionmaker(bind=engine)()
-    companies = session.query(db_company).all()
+    companies = session.query(company_c).all()
     return companies
 
 
--- job finder/objects.py
+++ job finder/objects.py
@@ -1,23 +1,46 @@
 import hashlib 
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, Boolean
 
-class job_c:
+base = declarative_base()
 
-    isremote = False
-    description = "None"
-    city = "None"
-    salary_low = 0
-    salary_high = 0
-    us_only = False
-    md5 = "None"
+class job_c(base):
 
-    def __init__(self, title, company ,url):
+    __tablename__ = "jobs"
+
+    md5 = Column(String, primary_key=True)
+    isremote = Column(Boolean)
+    company = Column(String)
+    description = Column(String)
+    city = Column(String)
+    salary_low = Column(Integer)
+    salary_high = Column(Integer)
+    us_only = Column(Boolean)
+
+    def __init__(self, title, company, url, description = "none", city = "none", isremote = False, salary_low = 0, salary_high = 0, us_only = False, md5 = "None"):
         self.title = title
-        self.company = company.name
+        try:
+            self.company = company.name
+        except:
+            self.company = company
         self.url = url
-        self.hash = hashlib.md5((title+company.name).encode()) 
+        if md5 is not "None":
+            self.md5 = md5
+        else:
+            self.md5 = hashlib.md5((title+company.name).encode()) 
+        self.isremote = isremote
+        self.description = description
+        self.city = city
+        self.salary_low = salary_low
+        self.salary_high = salary_high
+        self.us_only = us_only
 
 
 class company_c:
+
+    __tablename__ = "companies"
+
+    name = Column(String, primary_key=True)
 
     def __init__(self, name):
         self.name = name
