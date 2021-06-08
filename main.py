4d7d4be543b69c530f0c5b554154a408c61df1a7
Eric Warren <ericwarren99@hotmail.com>
2021-06-07 20:41:20 -0400

    debugging actions

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def getlinkedinCount(tech, location):
     options.headless = True
     driver = webdriver.Chrome(executable_path='chromedriver.exe', chrome_options=options)
     driver.get(f'https://www.linkedin.com/jobs/search?keywords={tech}&location={location}')
-
+    print(driver.page_source)
     count = driver.find_element_by_css_selector('#main-content > div > h1 > span.results-context-header__job-count').get_attribute('innerHTML')
     print(count)
     driver.close()
