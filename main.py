e700ce16c96105a1ab4ff7945e6939c73b98ac58
Eric Warren <ericwarren99@hotmail.com>
2021-06-07 20:19:02 -0400

    creating a workflow that updates a github page

---
+++ main.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+tech = 'splunk'
+location = 'United States'
+
+def getlinkedinCount(tech, location):
+    options = Options()
+    #options.headless = True
+    driver = webdriver.Chrome(executable_path='chromedriver.exe', chrome_options=options)
+    driver.get(f'https://www.linkedin.com/jobs/search?keywords={tech}&location={location}')
+
+    count = driver.find_element_by_css_selector('#main-content > div > h1 > span.results-context-header__job-count').get_attribute('innerHTML')
+    print(count)
+    driver.close()
+    return count
+
