422f3c8b5fd45c3dd307dc3a7065a64a0df305b5
Eric Warren <ericwarren99@hotmail.com>
2021-06-08 08:38:01 -0400

    added function that returns all the counts

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from os import listdir
 
-def getlinkedinCount(tech, location):
+def getlinkedinCount(tech):
+
     options = Options()
     options.headless = True
-    driver = webdriver.Chrome(executable_path='chromedriver.exe', chrome_options=options)
-    driver.get(f'https://googleweblight.com/?lite_url=https://www.linkedin.com/jobs/search?keywords={tech}&location={location}')
-    print(driver.page_source)
-    count = driver.find_element_by_css_selector('#main-content > div > h1 > span.results-context-header__job-count').get_attribute('innerHTML')
-    print(count)
+    driver = webdriver.Chrome(executable_path='chromedriver.exe', options=options)
+    driver.get(f'https://www.indeed.com/jobs?q={tech}&l=')
+    count = driver.find_element_by_css_selector('#searchCountPages').get_attribute('innerHTML')
+    count = int(count.strip()[10:-5].replace(',', ''))
     driver.close()
     return count
 
-tech = 'splunk'
-location = 'United States'
+def getAllSearch():
+    files = listdir('./searches')
+    searches = {}
+    for file in files:
+        with open(f'./searches/{file}') as f:
+            searches[file] = f.read().splitlines()
+    return searches
 
-getlinkedinCount(tech, location)
+
+def getAllCounts():
+    searches = getAllSearch()
+    allCounts = {}
+    for search in searches.items():
+        res= {}
+        for item in search[1]:
+            res[item] = getlinkedinCount(item)
+
+        allCounts[search[0]] = res
+    return allCounts
